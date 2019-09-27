a5a1234404e3b7692c0830b4ede00a656ccaed64
Carlos Gomes Cabral <carlos.gomescabral@gmail.com>
2019-09-26 21:25:10 -0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { routes } from './routes';
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+  scrollBehavior() {
+    return { x: 0, y: 0 }    
+  }  
+});
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import Home from './components/Home.vue';
+// import Users from './components/Users.vue'
+// import Dashboard from './components/Dashboard.vue'
+
+
+export const routes = [
+    { path: '/', component: Home, children: [
+        // { path: '', component: Dashboard },    
+        // { path: '/usuarios', component: Users }
+    ]}
+]
